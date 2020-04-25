5aceab7f802b2cc02ff1edf3fc109cc468a7abb1
chaoyyyy1111 <774881706@qq.com>
2020-04-25 22:40:37 +0800

    提交代码

--- day27/src/MyLinkedList.java
+++ day27/src/MyLinkedList.java
@@ -297,6 +297,39 @@ public Node partition(int x) {
         }
         return true;
     }
+    public boolean hasCycle() {
+        Node fast = this.head;
+        Node slow = this.head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public Node detectCycle() {
+        Node fast = this.head;
+        Node slow = this.head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast) {
+                break;
+            }
+        }
+        if(fast == null || fast.next == null) {
+            return null;
+    }
+        slow = this.head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
     public void clear() {
     this.head = null;
 }
--- day27/src/TestDemo.java
+++ day27/src/TestDemo.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class TestDemo {
     public static void main(String[] args) {
-        MyLinkedList myLinkedList = new MyLinkedList();
+       /* MyLinkedList myLinkedList = new MyLinkedList();
 
         for (int i = 0; i < 5; i++) {
             myLinkedList.addIndex(i,i);
@@ -12,8 +14,67 @@ public class TestDemo {
         System.out.println(ret.data);
         Node ret1 = myLinkedList.middleK(1);
         System.out.println(ret1.data);
+        */
 
 
 
     }
+    public static void creatCut(Node headA, Node headB) {
+        headA.next = headB.next.next;
+    }
+    public static Node getIntersectionNode(Node headA, Node headB) {
+        int lenA = 0;
+        int lenB = 0;
+        Node pl = headA;
+        Node ps = headB;
+        while(pl != null) {
+            pl = pl.next;
+            lenA++;
+        }
+        while(ps != null) {
+            ps = ps.next;
+            lenB++;
+        }
+        int len = lenA-lenB;
+        pl = headA;
+        ps = headB;
+        if(len < 0) {
+            pl = headB;
+            ps = headA;
+            len = lenB - lenA;
+        }
+        for (int i = 0; i < len; i++) {
+            pl = pl.next;
+        }
+        while(pl != ps) {
+            pl = pl.next;
+            ps = ps.next;
+        }
+        if(pl != null) {
+            return pl;
+        }
+        return null;
+    }
+    public Node mergeTwoLists(Node headA, Node headB) {
+        Node newHead = new Node(-1);
+        Node tmp = newHead;
+        while(headA != null && headB != null) {
+            if(headA.data < headB.data) {
+                tmp.next = headA;
+                tmp = tmp.next;
+                headA = headA.next;
+            } else {
+                tmp.next = headB;
+                tmp = tmp.next;
+                headB = headB.next;
+            }
+        }
+        if(headA != null) {
+            tmp.next = headA;
+        } else {
+            tmp.next = headB;
+        }
+        return newHead.next;
+    }
+
 }
