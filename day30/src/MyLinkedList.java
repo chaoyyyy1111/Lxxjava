b830f9ed9d9453d53c08184ff797d0f7b603a6fc
chaoyyyy1111 <774881706@qq.com>
2020-04-26 21:23:16 +0800

    提交代码

---
+++ day30/src/MyLinkedList.java
@@ -0,0 +1,127 @@
+
+
+class Node {
+    public int data;
+    public Node next;
+    public Node prev;
+    public Node(int data) {
+        this.data = data;
+    }
+
+}
+public class MyLinkedList {
+    public Node head;
+    public Node tail;
+public void addFirst(int data) {
+    Node node = new Node(data);
+    if(this.head == null) {
+        this.head = node;
+        this.tail = node;
+        return;
+    }
+    node.next = this.head;
+    this.head.prev = node;
+    this.head = node;
+}
+public void display() {
+    Node cur = this.head;
+    while(cur != null) {
+        System.out.print(cur.data +" ");
+        cur = cur.next;
+    }
+    System.out.println();
+}
+public void addLast(int data) {
+    Node node = new Node(data);
+    if(this.tail == null) {
+        this.head = node;
+        this.tail = node;
+        return;
+    }
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+}
+    public boolean contains(int key) {
+    Node cur = this.head;
+    while(cur != null) {
+        if(cur.data == key) {
+            return true;
+        }
+    cur = cur.next;
+    }
+    return false;
+}
+    public int size() {
+    Node cur = this.head;
+    int count = 0;
+    while(cur != null) {
+        count++;
+        cur = cur.next;
+    }
+    return count;
+}
+    private void checkIndex(int index) {
+        if(index < 0 || index > size()) {
+            throw new RuntimeException("index不合法");
+        }
+    }
+    private Node searchIndex(int index) {
+        Node cur = this.head;
+        while(index != 0) {
+            cur = cur.next;
+            index--;
+        }
+        return cur;
+    }
+    public void addIndex(int index,int data){
+        checkIndex(index);
+        if(index == 0) {
+            addFirst(data);
+            return;
+        }
+        if(index == size()) {
+            addLast(data);
+            return;
+        }
+        Node cur = searchIndex(index);
+        Node node = new Node(data);
+        cur.prev.next = node;
+        node.next = cur;
+        cur.prev = node;
+        node.prev = cur.prev;
+
+    }
+    public int remove(int key){
+        Node cur = this.head;
+        while (cur != null) {
+            if(cur.data == key) {
+                int oldData = cur.data;
+                if(cur == this.head) {
+                    this.head = this.head.next;
+                    this.head.prev = null;
+                }else {
+                     cur.prev.next = cur.next;
+                     if(cur.next != null) {
+                         cur.next.prev = cur.prev;
+                     } else {
+                         this.tail = cur.prev;
+                     }
+                }
+                return oldData;
+            }
+            cur = cur.next;
+        }
+        return -1;
+    }
+    public void clear() {
+    while(this.head != null) {
+        Node cur = this.head.next;
+        this.head.prev = null;
+        this.head.next = null;
+        this.head = cur;
+    }
+    this.tail = null;
+}
+
+}
---
+++ day30/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addIndex(0,1);
+        myLinkedList.addIndex(1,2);
+        myLinkedList.addIndex(2,3);
+        myLinkedList.addIndex(3,4);
+        myLinkedList.display();
+    }
+
+
+
+}
