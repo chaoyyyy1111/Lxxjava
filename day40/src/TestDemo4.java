9139a932ea6712b7fdb4f100e7b2b5447057dc79
chaoyyyy1111 <774881706@qq.com>
2020-05-28 22:24:59 +0800

    提交代码

--- day40/src/TestDemo4.java
+++ day40/src/TestDemo4.java
@@ -12,7 +12,20 @@ public class TestDemo4 {
         }
         return hashSet.iterator().next();
     }
-    public static void main(String[] args) {
+
+    public static void main5(String[] args) {
+        int i=0;
+        Integer j = new Integer(0);
+        System.out.println(i==j);
+        System.out.println(j.equals(i));
+        String a = "0.1";
+        String b = "1.1";
+        System.out.println(a.compareTo(b));
+        int[] arr = {1,3,5};
+
+
+    }
+    public static void main4(String[] args) {
         Random random = new Random();
         ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < 100000 ; i++) {
@@ -70,4 +83,45 @@ public class TestDemo4 {
             System.out.println(entry.getKey()+ " " + entry.getValue());
         }
     }
+    public static List<Integer> findClosestElements(int[] arr, int k, int x) {
+        Arrays.sort(arr);
+        int[][] ret = new int[arr.length][2];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i][0] = arr[i];
+            ret[i][1] = Math.abs(x - arr[i]);
+        }
+        for (int i = 1; i < ret.length; i++) {
+            int tmp = ret[i][1];
+            int t = ret[i][0];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (ret[j][1] > tmp) {
+                    ret[j+1][0] = ret[j][0];
+                    ret[j + 1][1] = ret[j][1];
+                } else {
+                    break;
+                }
+            }
+            ret[j + 1][0] = t;
+            ret[j + 1][1] = tmp;
+        }
+
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            list.add(ret[i][0]);
+
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        String str = "-2147483647";
+
+
+
+    }
+
+
 }
