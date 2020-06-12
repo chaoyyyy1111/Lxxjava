93c10b9c01db89a2e099452493d6fb0f53dc1914
chaoyyyy1111 <774881706@qq.com>
2020-06-12 22:22:18 +0800

    提交代码

--- day44/src/TestDemo.java
+++ day44/src/TestDemo.java
@@ -1,46 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class TestDemo {
-    public static int func(int n) {
-        int ret = 1;
-        while(n > 0) {
-            ret *= n;
-            n--;
+    public static int countWays(int x, int y) {
+        // write code here
+        if(x == 1 || y == 1) {
+            return 1;
         }
-        return ret;
+        return countWays(x-1,y) + countWays(x,y-1);
     }
-    public static void main1(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        while(scan.hasNext()) {
-            int n = scan.nextInt();
-            int ret = func(n);
-
-            System.out.println(ret);
-        }
-
-
-    }
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        List<Character> list = new ArrayList<>();
-        if(n == 0) {
-            list.add((char)(48));
-        }
-        while(n > 0) {
-            list.add((char)(n % 10 + 48));
-            n /= 10;
-        }
-        char[] cs = new char[list.size()];
-        int i = 0;
-        for (char c :list) {
-            cs[i++] = c;
-        }
-        String str = new String(cs);
-        System.out.println(str);
+        System.out.println(countWays(3, 3));
+
     }
 }
 
