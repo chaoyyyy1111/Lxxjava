de8abe736a220a352dd426654e096beacd4ffe74
chaoyyyy1111 <774881706@qq.com>
2020-07-21 23:22:05 +0800

    提交代码

--- day59/src/TestDemo.java
+++ day59/src/TestDemo.java
@@ -1,69 +1,64 @@
 import java.util.*;
-
 public class TestDemo {
-    public static void main(String[] args) {
-        Map<Character,Character> map = new HashMap<>();
-        map.put('A','2');
-        map.put('B','2');
-        map.put('C','2');
-        map.put('D','3');
-        map.put('E','3');
-        map.put('F','3');
-        map.put('G','4');
-        map.put('H','4');
-        map.put('I','4');
-        map.put('J','5');
-        map.put('K','5');
-        map.put('L','5');
-        map.put('M','6');
-        map.put('N','6');
-        map.put('O','6');
-        map.put('P','7');
-        map.put('Q','7');
-        map.put('R','7');
-        map.put('S','7');
-        map.put('T','8');
-        map.put('U','8');
-        map.put('V','8');
-        map.put('W','9');
-        map.put('X','9');
-        map.put('Y','9');
-        map.put('Z','9');
-        Scanner scan = new Scanner(System.in);
-        while(scan.hasNext()) {
-            int n = scan.nextInt();
-            Set<String> set = new HashSet<>();
-            for (int i = 0; i < n ; i++) {
-                String str = scan.next();
-                StringBuffer sb = new StringBuffer();
-                for (int j = 0; j < str.length(); j++) {
-                    char c = str.charAt(j);
-                    if(c >= '0' && c <= '9') {
-                        sb.append(c);
-                    }
-                    if(c >= 'A' && c <= 'Z') {
-                        sb.append(map.get(c));
-                    }
-                }
-                set.add(sb.toString());
-            }
-
-            PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>() {
-                @Override
-                public int compare(String o1, String o2) {
-                    return Integer.parseInt(o1)-Integer.parseInt(o2);
-                }
-            });
-            for (String s:set) {
-                pq.offer(s);
-            }
-            while(!pq.isEmpty()) {
-                String s = pq.poll();
-                String ret = s.substring(0,3)+"-"+s.substring(3);
-                System.out.println(ret);
-            }
-            System.out.println();
-        }
+    public static String func(String s) {
+        StringBuffer sb = new StringBuffer(s);
+        return sb.reverse().toString();
     }
+    public static boolean func1(String str,String str1) {
+            Map<Character,Integer> map = new HashMap<>();
+            for(int i = 0;i < str1.length();i++) {
+                if(!map.containsKey(str1.charAt(i))) {
+                    map.put(str1.charAt(i),1);
+                } else {
+                    map.put(str1.charAt(i),map.get(str1.charAt(i))+1);
+                }
+            }
+            for(int i = 0;i < str.length();i++) {
+                if((!str1.contains(str.charAt(i)+""))||map.get(str.charAt(i))<1) {
+                    char c = str.charAt(i);
+                    for(int j = 0; j <= str1.length();j++) {
+                        StringBuffer sb = new StringBuffer(str1);
+                        if(sb.insert(j,c).toString().equals(func(str))) {
+                            //System.out.println("YES");
+                            return true;
+                        }
+                    }
+                } else {
+                    map.put(str.charAt(i),map.get(str.charAt(i))-1);
+                }
+            }
+            //System.out.println("NO");
+            return false;
 
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        if(s.length() % 2 == 0) {
+            String str = s.substring(0,s.length()/2);
+            String str1 = s.substring(s.length()/2+1);
+            boolean f1 = func1(str,str1);
+            str = s.substring(s.length());
+            str1 = s.substring(0,s.length()-1);
+            boolean f2 = func1(str,str1);
+            if(f1 || f2) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        } else {
+            String str = s.substring(0,s.length()/2+1);
+            String str1 = s.substring(s.length()/2+1);
+            boolean f1 = func1(str,str1);
+            str = s.substring(s.length()/2);
+            str1 = s.substring(0,s.length()/2);
+            boolean f2 = func1(str,str1);
+            if(f1 || f2) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+    }
 }
